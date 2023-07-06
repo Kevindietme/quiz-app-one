9cee516ec454408b8fb9f100ffdec535f7f10c62
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-06 10:43:18 -0400

    first commit

---
+++ src/components /QuizApp/Card.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Card() {
+    return(
+        <div className="card">
+        <p>1/3</p>
+        <h2>Questions</h2>
+        <p>Options</p>
+        </div>
+    )
+}
